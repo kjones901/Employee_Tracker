087c3cec267ef3e2b68eeb1074d10a76848b7efb
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-05-15 18:36:13 -0600

    continued working on my updateemployee function. Added an updated README as well as a screenshot of the application.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,9 +4,9 @@ VALUES (001, "Department 1"),
        (003, "Department 3");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (001, "boss", 100000.00, 001),
-       (002, "manager", 60000.00, 002),
-       (003, "worker", 20000.00, 003);
+VALUES (001, "manager", 100000, 001),
+       (002, "hr", 60000, 002),
+       (003, "worker", 20000, 003);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (001, "Big", "Boss", 001, null),
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 /** @format */
 const mysql = require("mysql2");
 const { prompt } = require("inquirer");
-const lists = require("./utils/navigation.js");
-const { addDepartment, depQuestions, addRole } = require("./utils/modification.js");
 require("console.table");
 
 const db = mysql.createConnection(
@@ -28,10 +26,10 @@ function selectionFunction() {
         "Add A Role",
         "Add An Employee",
         "Update An Employee",
-        "Exit",
       ],
     },
   ];
+
   prompt(lists).then((input) => {
     if (input.main === "View All Departments") {
       db.query("SELECT * FROM DEPARTMENT;", function (err, results) {
@@ -61,16 +59,39 @@ function selectionFunction() {
       });
     }
     if (input.main === "Add A Department") {
-      async function add() {
-        const answer = await prompt(depQuestions);
-        addDepartment(answer);
-      }
-      add();
+      addDepartment();
     }
     if (input.main === "Add A Role") {
       addRole();
     }
+    if (input.main === "Add An Employee") {
+      addEmployee();
+    }
+    if (input.main === "Update An Employee") {
+      updateEmployee();
+    }
   });
+
+  async function addDepartment() {
+    prompt([
+      {
+        type: "input",
+        name: "department",
+        message: "What would you like to call the new Department?",
+      },
+    ]).then((input) => {
+      const newDepartment = { name: input.department };
+      db.query(
+        "INSERT INTO department (name) VALUES(?);",
+        newDepartment.name,
+        (error) => {
+          if (error) throw error;
+          console.log(`${newDepartment.name} was added.`);
+        }
+      );
+      selectionFunction();
+    });
+  }
 
   async function addRole() {
     await db
@@ -112,8 +133,105 @@ function selectionFunction() {
         });
       });
   }
+
+  async function addEmployee() {
+    const [roleResults, employeeResults] = await Promise.all([
+      db.promise().query("SELECT * FROM ROLE;"),
+      db.promise().query("SELECT * FROM EMPLOYEE;"),
+    ]);
+    const roles = roleResults[0].map(({ id, title }) => ({
+      value: id,
+      name: title,
+    }));
+    const managers = employeeResults[0].map(({ id, first_name, last_name }) => ({
+      value: id,
+      name: `${first_name} ${last_name}`,
+    }));
+    prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "Select the new employee's role.",
+        choices: roles,
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Who is the new employee's manager?",
+        choices: managers,
+      },
+    ]).then((input) => {
+      const newEmployee = {
+        first_name: input.first_name,
+        last_name: input.last_name,
+        role_id: input.role,
+        manager_id: input.manager,
+      };
+      db.query("INSERT INTO employee SET ?", newEmployee, (error) => {
+        if (error) throw error;
+        console.log(`${newEmployee.first_name} ${newEmployee.last_name} was added.`);
+      });
+      selectionFunction();
+    });
+  }
+
+  async function updateEmployee() {
+    const [roleResults, employeeResults] = await Promise.all([
+      db.promise().query("SELECT * FROM ROLE;"),
+      db.promise().query("SELECT * FROM EMPLOYEE;"),
+    ]);
+    const roles = roleResults[0].map(({ id, title }) => ({
+      value: id,
+      name: title,
+    }));
+    const employees = employeeResults[0].map(
+      ({ id, first_name, last_name, role_id }) => ({
+        value: id,
+        role_id: role_id,
+        name: `${first_name} ${last_name}`,
+      })
+    );
+    prompt(
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Select an employee to update",
+        choices: employees,
+      },
+      {
+        type: "list",
+        name: "newRole",
+        message: "Select the employee's new role",
+        choices: roles,
+      }
+    ).then((input) => {
+      const updatedEmployee = {
+        role_id: input.newRole,
+        id: input.employeeId,
+      };
+      db.query(
+        "UPDATE employee SET role_id = ? WHERE id = ?",
+        [updatedEmployee.role_id, updatedEmployee.id],
+        (error) => {
+          if (error) throw error;
+          console.log(
+            `${updatedEmployee.first_name} ${updatedEmployee.last_name} was updated.`
+          );
+        }
+      );
+      selectionFunction();
+    });
+  }
 }
-
-
 
 init();
